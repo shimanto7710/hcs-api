ff621d224bda7ad9cd8471653a0bf9b73dea07df
Shimanto Ahmed <shimanto7710@gmail.com>
2020-06-22 18:47:17 +0600

    login api done

--- src/main/kotlin/com/rookie/hcsapi/repo/UserRepository.kt
+++ src/main/kotlin/com/rookie/hcsapi/repo/UserRepository.kt
@@ -5,7 +5,9 @@ package com.rookie.hcsapi.repo
 import com.rookie.hcsapi.model.UserModel
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository: JpaRepository<UserModel, Long> {
 
     @Query(value = "SELECT * FROM user where phone_number = ?1", nativeQuery = true)
