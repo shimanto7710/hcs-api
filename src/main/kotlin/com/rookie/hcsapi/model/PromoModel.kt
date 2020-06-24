c6aed3ed76b34a393c559b8eafd2add5364464e8
Shimanto Ahmed <shimanto7710@gmail.com>
2020-06-24 23:43:49 +0600

    working with promo api

--- src/main/kotlin/com/rookie/hcsapi/model/PromoModel.kt
+++ src/main/kotlin/com/rookie/hcsapi/model/PromoModel.kt
@@ -1,37 +1,37 @@
-//package com.rookie.hcsapi.model
-//
-//import java.io.Serializable
-//import javax.persistence.*
-//
-//
-//@Entity
-//@Table(name = "promo")
-//class PromoModel(
-//        @Id
-//        @GeneratedValue(strategy = GenerationType.IDENTITY)
-//        @Column(name = "id")
-//        var id: Long? = null,
-//        @Column(name = "title")
-//        var title: String? = null,
-//        @Column(name = "description")
-//        var description: String? = null,
-//        @Column(name = "start_time")
-//        var startTimeStamp: String? = null,
-//        @Column(name = "end_time")
-//        var endTimeStamp: String? = null,
-//        @Column(name = "is_active")
-//        var isActive: Int? = null,
-//        @Column(name = "discount")
-//        var discount: Double? = null,
-//        @Column(name = "image")
-//        var imgUrl: String? = null
-////        @ManyToOne(fetch = FetchType.EAGER)
-////        @JoinColumn(name = "service_id")
-////        var service: ServiceModel? = null
-//
-//
-//) : Serializable {
-//    override fun toString(): String {
-//        return "PromoModel(id=$id, title=$title, description=$description, startTimeStamp=$startTimeStamp, endTimeStamp=$endTimeStamp, isActive=$isActive, discount=$discount, imgUrl=$imgUrl)"
-//    }
-//}
+package com.rookie.hcsapi.model
+
+import java.io.Serializable
+import javax.persistence.*
+
+
+@Entity
+@Table(name = "promo")
+class PromoModel(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        var id: Long? = null,
+        @Column(name = "title")
+        var title: String? = null,
+        @Column(name = "description")
+        var description: String? = null,
+        @Column(name = "start_time")
+        var startTimeStamp: String? = null,
+        @Column(name = "end_time")
+        var endTimeStamp: String? = null,
+        @Column(name = "is_active")
+        var isActive: Int? = null,
+        @Column(name = "discount")
+        var discount: Double? = null,
+        @Column(name = "image")
+        var imgUrl: String? = null
+//        @ManyToOne(fetch = FetchType.EAGER)
+//        @JoinColumn(name = "service_id")
+//        var service: ServiceModel? = null
+
+
+) : Serializable {
+    override fun toString(): String {
+        return "PromoModel(id=$id, title=$title, description=$description, startTimeStamp=$startTimeStamp, endTimeStamp=$endTimeStamp, isActive=$isActive, discount=$discount, imgUrl=$imgUrl)"
+    }
+}
--- src/main/kotlin/com/rookie/hcsapi/repo/PromoRepo.kt
+++ src/main/kotlin/com/rookie/hcsapi/repo/PromoRepo.kt
@@ -1,10 +1,10 @@
-//package com.rookie.hcsapi.repo
-//
-//import com.rookie.hcsapi.model.PromoModel
-//import com.rookie.hcsapi.model.ServiceModel
-//import org.springframework.data.jpa.repository.JpaRepository
-//import org.springframework.stereotype.Repository
-//
-//@Repository
-//interface PromoRepo : JpaRepository<PromoModel, Long> {
-//}
+package com.rookie.hcsapi.repo
+
+import com.rookie.hcsapi.model.PromoModel
+import com.rookie.hcsapi.model.ServiceModel
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PromoRepo : JpaRepository<PromoModel, Long> {
+}
