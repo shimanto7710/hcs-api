48ada3eb68e7ad56f11617bcb7b8cfd88b48c1d2
Shimanto Ahmed <shimanto7710@gmail.com>
2020-06-24 17:14:36 +0600

    working with promo api

--- src/main/kotlin/com/rookie/hcsapi/model/PromoModel.kt
+++ src/main/kotlin/com/rookie/hcsapi/model/PromoModel.kt
@@ -5,7 +5,7 @@ import javax.persistence.*
 
 
 @Entity
-@Table(name = "promo_table")
+@Table(name = "promo")
 class PromoModel(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
