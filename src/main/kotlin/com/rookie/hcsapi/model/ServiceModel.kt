ebf56bd851966a77da629af698af630357ec1068
Shimanto Ahmed <shimanto7710@gmail.com>
2020-06-24 14:33:13 +0600

    service api done

--- src/main/kotlin/com/rookie/hcsapi/model/ServiceModel.kt
+++ src/main/kotlin/com/rookie/hcsapi/model/ServiceModel.kt
@@ -1,32 +1,32 @@
-package com.rookie.hcsapi.model
-
-import java.io.Serializable
-import javax.persistence.*
-
-
-@Entity
-@Table(name = "service_table")
-class ServiceModel(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "id")
-        var id: Long? = null,
-        @Column(name = "name")
-        var serviceName: String?=null,
-        @Column(name = "cost")
-        var serviceCost: Double?=null,
-        @Column(name = "distance")
-        var distanceCost: Double?=null,
-        @Column(name = "description")
-        var serviceDes: String?=null,
-        @Column(name = "type")
-        var type: String?=null,
-        @Column(name = "image")
-        var image: String?=null
-
-) : Serializable {
-
-    override fun toString(): String {
-        return "ServiceModel(id=$id, serviceName='$serviceName', serviceCost=$serviceCost, distanceCost=$distanceCost, serviceDes='$serviceDes', type='$type', image='$image')"
-    }
-}
+//package com.rookie.hcsapi.model
+//
+//import java.io.Serializable
+//import javax.persistence.*
+//
+//
+//@Entity
+//@Table(name = "service_table")
+//class ServiceModel(
+//        @Id
+//        @GeneratedValue(strategy = GenerationType.IDENTITY)
+//        @Column(name = "id")
+//        var id: Long? = null,
+//        @Column(name = "name")
+//        var serviceName: String?=null,
+//        @Column(name = "cost")
+//        var serviceCost: Double?=null,
+//        @Column(name = "distance")
+//        var distanceCost: Double?=null,
+//        @Column(name = "description")
+//        var serviceDes: String?=null,
+//        @Column(name = "type")
+//        var type: String?=null,
+//        @Column(name = "image")
+//        var image: String?=null
+//
+//) : Serializable {
+//
+//    override fun toString(): String {
+//        return "ServiceModel(id=$id, serviceName='$serviceName', serviceCost=$serviceCost, distanceCost=$distanceCost, serviceDes='$serviceDes', type='$type', image='$image')"
+//    }
+//}
